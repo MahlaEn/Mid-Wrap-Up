f682dde149b5ad875c5dafc78444fa4284c2dfc7
Mahla <MahlaEntezari8926@gmail.com>
2023-04-19 17:42:00 +0430

    done!

--- src/main/java/Lecture6Exercises.java
+++ src/main/java/Lecture6Exercises.java
@@ -1,56 +1,72 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Lecture6Exercises {
-
-    /*
-     *   implement a function that takes an array of int and return sum of
-     *   elements at even positions
-     *   lecture 6 page  16
-     */
     public long calculateEvenSum(int[] arr) {
-        return 0L;
+        long ans=0;
+        for(int i=0;i<arr.length;i+=2){
+            ans+=arr[i];
+        }
+        return ans;
     }
-
-    /*
-     *   implement a function that takes an array of int and return that
-     *   array in reverse order
-     *   lecture 6 page 16
-     */
     public int[] reverseArray(int[] arr) {
-        return null;
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=arr[arr.length-1-i];
+        }
+        return ans;
     }
-
-    /*
-     *   implement a function that calculate product of two 2-dim matrices
-     *   lecture 6 page 21
-     */
-    public double[][] matrixProduct(double[][] m1, double[][] m2) throws RuntimeException {
-        return null;
+    public double[][] matrixProduct(double[][] a, double[][] b) throws RuntimeException {
+        int n=a.length;
+        int m=a[0].length;
+        int x=b.length;
+        int y=b[0].length;
+        if(m!=x){
+            throw new IllegalArgumentException("Error!");
+        }
+        double ans[][]=new double[n][y];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<y;j++){
+                for(int l=0;l<m;l++){
+                    ans[i][j]+=a[i][l]*b[l][j];
+                }
+            }
+        }
+        return ans;
     }
-
-    /*
-     *   implement a function that return array list of array list of string
-     *   from a 2-dim string array
-     *   lecture 6 page 30
-     */
     public List<List<String>> arrayToList(String[][] names) {
-        return null;
+        List<List<String>>ans=new LinkedList<>();
+        int x= names.length;
+        int y=names[0].length;
+        for(int i=0;i<x;i++){
+            List<String>temp=new LinkedList<>();
+            for(int j=0;j<y;j++){
+                temp.add(names[i][j]);
+            }
+            ans.add(temp);
+        }
+        return ans;
     }
-
-    /*
-     *   implement a function that return a list of prime factor of integer n
-     *   in ascending order
-     *   lecture 6 page 30
-     */
     public List<Integer> primeFactors(int n) {
-        return null;
+        List<Integer>ans=new LinkedList<>();
+        for(int i=2;i<=n;i++){
+            if(n%i==0) {
+                ans.add(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        return ans;
     }
-
-    /*
-     *   implement a function that return a list of words in a given string
-     *   lecture 6 page 30
-     */
     public List<String> extractWord(String line) {
-        return null;
+        List<String>ans=new LinkedList<>();
+        String[] temp=line.split("\\W+");
+        for(int i=0;i< temp.length;i++){
+            ans.add(temp[i]);
+            System.out.println(temp[i]);
+        }
+        return ans;
     }
 }
